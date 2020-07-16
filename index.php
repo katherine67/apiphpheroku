635df14e2ff0b94241837540a498f3a41977c9d7
katherine67 <54502836+katherine67@users.noreply.github.com>
2020-07-16 15:47:18 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <body>
      
 <a href="config.php">configurar</a><br>
+     <button name="button"><a href="config.php">configurar</a></button>
 <a href="delete.php">borrar</a><br>
 <a href="insert.php">Insertar</a><br>
 <a href="read.php">read</a><br>
